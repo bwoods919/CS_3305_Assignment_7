f25d4e611aea3429a36020b4294ef33199b6b785
brendanwoods <bwoods51501@gmail.com>
2022-10-31 21:40:59 -0400

    Changes

--- src/PQ_heap.java
+++ src/PQ_heap.java
@@ -7,39 +7,44 @@
 
 // Generic code for class priority_queue_heap for Assignment 7
 
-public class PQ_heap
-{	
-	// Constructor method
-   PQ_heap() {};
-	
-	// Return true if priority queue is empty; otherwise return false
+public class PQ_heap <E extends Comparable<E>> {
+
+    private Heap<E> heap = new Heap();
+
+    // Constructor method
+    PQ_heap() {
+        heap.add(object);
+    };
+
+    // Return true if priority queue is empty; otherwise return false
     public boolean is_empty() {
-       return true;
-   };
-	
-	// Return true if priority queue is full; otherwise return false
-   public boolean is_full() {
-       return true;
-   };
+        return heap.getSize() == 0;
+    };
 
-	// Return (don't remove) the front element from the priority queue
-	// Precondition: priority queue is not empty.
-   public int front() {
-       return 1;
-   };
+    // Return true if priority queue is full; otherwise return false
+    public boolean is_full() {
+        return heap.getSize() == 100;
+    };
 
-	// return number of elements in the queue
-   public int size() {
-       return 0;
-   };
-   
-	// Remove the largest value from this priority queue and return it.
-	// Precondition: priority queue is not empty.
-   public int dequeue() {
-       return 3;
-   };
+    // Return (don't remove) the front element from the priority queue
+    // Precondition: priority queue is not empty.
+    public E front() {
+        return null;
+    };
 
-	// Inserts the 'value' into the priority queue.
-	// Precondition: priority queue is not full
-   public void enqueue(int value) {};
+    // return number of elements in the queue
+    public int size() {
+        return 0;
+    };
+
+    // Remove the largest value from this priority queue and return it.
+    // Precondition: priority queue is not empty.
+    public E dequeue() {
+        return heap.remove();
+    };
+    // Inserts the 'value' into the priority queue
+    // Precondition: priority queue is not full
+    public void enqueue(E value) {
+        heap.add(value);
+    };
 };
--- src/TestPQH.java
+++ src/TestPQH.java
@@ -14,6 +14,39 @@ public class TestPQH {
 
         int option;
 
+        boolean firstRun = false;
+        boolean data = true;
+
+        String inputStr = "";
+
+        PQ_heap<String> stringHeap = new PQ_heap<String>();
+        PQ_heap<Integer> intHeap = new PQ_heap<Integer>();
+
+        while (!firstRun) {
+
+            printMenu();
+            option = input.nextInt();
+
+            if (option != 0) {
+                System.out.println("Invalid Selection, first you must choose option 0!");
+            } else {
+                System.out.print("\nEnter the data type(integer/string): ");
+                inputStr = input.next();
+
+                if (inputStr.equals("integer") || inputStr.equals("Integer") ||
+                        inputStr.equals("int") || inputStr.equals("Int") ||
+                        inputStr.equals("i") || inputStr.equals("I")) {
+                    data = false;
+                } else if (inputStr.equals("string") || inputStr.equals("String") ||
+                        inputStr.equals("str") || inputStr.equals("Str") ||
+                        inputStr.equals("s") || inputStr.equals("S")) {
+                    data = true;
+                } else
+                    System.out.println("Please select either integer or string.");
+                firstRun = true;
+            }
+        }
+
         while (true) {
 
             printMenu();
@@ -22,15 +55,47 @@ public class TestPQH {
             switch (option) {
 
                 case 0: // Enter Queue Type (integer or string)
+                    System.out.print("\nEnter the data type(integer/string): ");
+                    inputStr = input.next();
+
+                    if (inputStr.equals("integer") || inputStr.equals("Integer") ||
+                            inputStr.equals("int") || inputStr.equals("Int") ||
+                            inputStr.equals("i") || inputStr.equals("I")) {
+                        data = false;
+                    } else if (inputStr.equals("string") || inputStr.equals("String") ||
+                            inputStr.equals("str") || inputStr.equals("Str") ||
+                            inputStr.equals("s") || inputStr.equals("S")) {
+                        data = true;
+                    }
+                    else
+                        System.out.println("Please select either integer or string.");
                     break;
+
                 case 1: // Enqueue Element
+
+                    if (data) { // String
+                        System.out.print("Enter a string to enqueue: ");
+                        stringHeap.enqueue(input.next());
+                    } else { // Integer
+                        System.out.print("Enter an integer to enqueue: ");
+                        intHeap.enqueue(input.nextInt());
+                    }
                     break;
+
                 case 2: // Dequeue Element
+                    if (data) // String
+                        stringHeap.dequeue();
+                    else // Integer
+                        intHeap.dequeue();
                     break;
+
                 case 3: // Check is_Full
+                    if (data) // String
+                        System.out.println("Is string Heap full? " + stringHeap.is_full());
+                    else // integer
+                        System.out.println("Is integer Heap full? " + intHeap.is_full());
                     break;
-                case 4: // Check is_Empty
-                    break;
+
                 case 5: // Print PQueue Size
                     break;
                 case 6: // Display Front Element
